a7a480a8d67c355802a703a771877dda99d4fc57
Reza <light.reza@gmali.com>
2026-02-19 11:12:42 +0330

    mtproto: add internal seq_no generator and tests

--- mtproto/seqno_test.go
+++ mtproto/seqno_test.go
@@ -17,3 +17,23 @@ func TestSeqNoGenerator(t *testing.T) {
 		t.Fatalf("expected value 1, got %d", got)
 	}
 }
+
+func TestSeqNoGeneratorOddEvenRules(t *testing.T) {
+	gen := NewSeqNoGenerator(0)
+
+	first := gen.Next(true)
+	if first%2 == 0 {
+		t.Fatalf("expected content-related seq_no to be odd, got %d", first)
+	}
+	second := gen.Next(false)
+	if second%2 != 0 {
+		t.Fatalf("expected non-content seq_no to be even, got %d", second)
+	}
+	third := gen.Next(true)
+	if third%2 == 0 {
+		t.Fatalf("expected content-related seq_no to be odd, got %d", third)
+	}
+	if gen.Value() != 2 {
+		t.Fatalf("expected content-related count 2, got %d", gen.Value())
+	}
+}
